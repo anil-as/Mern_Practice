01cb809139ed17de0227bec967e44151066924bb
anil-as <axpt74@gmail.com>
2025-07-23 10:13:26 +0530

    Completed env and db connection setups

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,17 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
 
 dotenv.config()
+
 const app = express()
+const PORT = process.env.PORT || 3000
+
 connectDB();
 
 app.use("/api/notes",notesRoutes);
 
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(PORT, () => {
+  console.log('Server started on port:',PORT);
 });
 
 
